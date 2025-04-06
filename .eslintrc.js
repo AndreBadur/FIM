5a506ed6c197b4de23cf314d70112a33f0dcca3f
Andre Badur <andrebadur123@gmail.com>
2025-04-06 16:39:54 -0300

    ADDING - prisma package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,19 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      project: './tsconfig.json',
-      sourceType: 'module',
-    },
-    plugins: ['@typescript-eslint', 'prettier'],
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:prettier/recommended',
-    ],
-    rules: {
-      'no-console': 'warn',
-      '@typescript-eslint/no-explicit-any': 'warn',
-      'prettier/prettier': 'error',
-    },
-  };
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'no-console': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'prettier/prettier': 'error',
+  },
+}
