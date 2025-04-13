2c55b405722bd4d8260789222f45dbca7dd90dd9
Vitor Hugo Paschuanelli <vitorhpaschuanelli@gmail.com>
2025-04-12 22:12:44 -0300

    Atualização

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,12 +2,24 @@ export default function Menu() {
   return (
     <div className="bg-gray-600 w-48 h-screen flex flex-col items-center pt-4">
       <h1 className="font-bold text-white mb-2">MENU</h1>
-      <a href="#" className="text-white mt-6">OPÇÃO</a>
-      <a href="#" className="text-white mt-6">OPÇÃO</a>
-      <a href="#" className="text-white mt-6">OPÇÃO</a>
-      <a href="#" className="text-white mt-6">OPÇÃO</a>
-      <a href="#" className="text-white mt-6">OPÇÃO</a>
-      <a href="#" className="text-white mt-6">OPÇÃO</a>
+      <a href="#" className="text-white mt-6">
+        OPÇÃO
+      </a>
+      <a href="#" className="text-white mt-6">
+        OPÇÃO
+      </a>
+      <a href="#" className="text-white mt-6">
+        OPÇÃO
+      </a>
+      <a href="#" className="text-white mt-6">
+        OPÇÃO
+      </a>
+      <a href="#" className="text-white mt-6">
+        OPÇÃO
+      </a>
+      <a href="#" className="text-white mt-6">
+        OPÇÃO
+      </a>
     </div>
-  );
+  )
 }
