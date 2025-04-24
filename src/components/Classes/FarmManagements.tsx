8130243ffc37dc8b2814ff965093dd541efbaa65
Andre Badur <andrebadur123@gmail.com>
2025-04-24 20:24:38 -0300

    Farm Routes and Class Methods

--- src/components/Classes/FarmManagements.tsx
+++ src/components/Classes/FarmManagements.tsx
@@ -1,3 +1,13 @@
+type farmType = {
+    cnpj: string
+    corporate_name: string
+    created_at: string
+    id_address: number
+    id_farm: number
+    id_farmer: number
+    updated_at: number
+}
+
 export class FarmManagement {
     constructor() {}
 
@@ -27,14 +37,69 @@ export class FarmManagement {
         }
     }
 
-    public async FarmListAll(): Promise<Response[]> {
-        console.log('farm list')
+    public async FarmListAll(): Promise<farmType[]> {
         const response: Response = await fetch('api/farms')
 
         if (response.ok) {
             return response.json()
         } else {
             throw new Error('Error handling the Farm GET method')
+        }
+    }
+
+    public async updateFarm(
+        idAddress: string,
+        farmCnpj: string,
+        corporateName: string,
+        id: string,
+    ): Promise<Response> {
+        const response: Response = await fetch(`api/farms/${Number(id)}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                idAddress: idAddress,
+                farmCnpj: farmCnpj,
+                corporateName: corporateName,
+            }),
+        })
+
+        if (response.ok) {
+            return response
+        } else {
+            throw new Error('Error handling the Farm POST method')
+        }
+    }
+
+    public async findUniqueFarm(id: string): Promise<farmType> {
+        try {
+            const response: Response = await fetch(`api/farms/${Number(id)}`, {
+                method: 'GET',
+            })
+            if (response.ok) {
+                return response.json()
+            } else {
+                throw response.status
+            }
+        } catch (error) {
+            throw error
+        }
+    }
+
+    public async deleteUniqueFarm(id: string): Promise<Request> {
+        console.log('deleteUniqueFarm')
+        try {
+            const response: Response = await fetch(`api/farms/${Number(id)}`, {
+                method: 'DELETE',
+            })
+            if (response.ok) {
+                return response.json()
+            } else {
+                throw response.status
+            }
+        } catch (error) {
+            throw error
         }
     }
 }
--- src/components/FarmCreation.tsx
+++ src/components/FarmCreation.tsx
@@ -3,23 +3,10 @@
 import { useState } from 'react'
 import { FarmManagement } from './Classes/FarmManagements'
 
-type farmType = [
-    {
-        cnpj: string
-        corporate_name: string
-        created_at: string
-        id_address: number
-        id_farm: number
-        id_farmer: number
-        updated_at: number
-    },
-]
-
 const farmManagement = new FarmManagement()
-const farmList = JSON.stringify(await farmManagement.FarmListAll())
-const parseFarmList: farmType = JSON.parse(farmList)
 
-parseFarmList.map((value) => {
+const farmList = await farmManagement.FarmListAll()
+farmList.map((value) => {
     console.log(value.id_farm, value.corporate_name)
 })
 
@@ -30,11 +17,13 @@ export default function FarmCreation() {
     const [corporateName, setCorporateName] = useState<string>('')
 
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
+        <div className="flex flex-col items justify-center h-screen">
+            <h1>CRIAR</h1>
             <form
                 className="flex flex-col items-center justify-center h-screen"
                 onSubmit={(e) => {
                     e.preventDefault()
+                    farmManagement.FarmCreation(idFarmer, idAddress, cnpj, corporateName)
                 }}
             >
                 <h1>ID FARMER</h1>
@@ -77,9 +66,113 @@ export default function FarmCreation() {
                 <button
                     className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
                     type="submit"
-                    onClick={farmManagement.FarmCreation(idFarmer, idAddress, cnpj, corporateName)}
                 >
                     CRIAR FAZENDA
+                </button>
+            </form>
+
+            <h1>ATUALIZAR</h1>
+            <form
+                className="flex flex-col items-center justify-center h-screen"
+                onSubmit={(e) => {
+                    e.preventDefault()
+                    farmManagement.updateFarm(idAddress, cnpj, corporateName, idFarmer)
+                }}
+            >
+                <h1>ID FARMER</h1>
+                <input
+                    className="border-black"
+                    type="text"
+                    name="setIdFamer"
+                    onChange={(e) => {
+                        setIdFarmer(e.target.value)
+                    }}
+                />
+
+                <h1>ID ADDRESS</h1>
+                <input
+                    className="border-black"
+                    type="text"
+                    onChange={(e) => {
+                        setIdAddress(e.target.value)
+                    }}
+                />
+
+                <h1>CNPJ</h1>
+                <input
+                    className="border-black"
+                    type="text"
+                    onChange={(e) => {
+                        setCnpj(e.target.value)
+                    }}
+                />
+
+                <h1>CORPORATE NAME</h1>
+                <input
+                    className="border-black"
+                    type="text"
+                    onChange={(e) => {
+                        setCorporateName(e.target.value)
+                    }}
+                />
+
+                <button
+                    className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+                    type="submit"
+                >
+                    ATUALIZAR FAZENDA
+                </button>
+            </form>
+
+            <h1>BUSCAR UMA UNICA FAZENDA</h1>
+            <form
+                className="flex flex-col items-center justify-center h-screen"
+                onSubmit={async (e) => {
+                    e.preventDefault()
+                    const teste = await farmManagement.findUniqueFarm(idFarmer)
+                    console.log(teste.corporate_name)
+                }}
+            >
+                <h1>ID FARMER</h1>
+                <input
+                    className="border-black"
+                    type="text"
+                    name="setIdFamer"
+                    onChange={(e) => {
+                        setIdFarmer(e.target.value)
+                    }}
+                />
+                <button
+                    className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+                    type="submit"
+                >
+                    BUSCAR FAZENDA
+                </button>
+            </form>
+
+            <h1>DELETAR UMA UNICA FAZENDA</h1>
+            <form
+                className="flex flex-col items-center justify-center h-screen"
+                onSubmit={async (e) => {
+                    e.preventDefault()
+                    const teste = await farmManagement.deleteUniqueFarm(idFarmer)
+                    console.log(teste)
+                }}
+            >
+                <h1>ID FARMER</h1>
+                <input
+                    className="border-black"
+                    type="text"
+                    name="setIdFamer"
+                    onChange={(e) => {
+                        setIdFarmer(e.target.value)
+                    }}
+                />
+                <button
+                    className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+                    type="submit"
+                >
+                    DELETAR FAZENDA
                 </button>
             </form>
         </div>
