6729a821da5ebe5f55d3671a0a1974996aa29750
VITOR HUGO PASCHUANELLI <vitor.paschuanelli@fatec.sp.gov.br>
2025-05-07 09:12:08 -0300

    Update: Menu

--- src/app/funcionaryArea/page.tsx
+++ src/app/funcionaryArea/page.tsx
@@ -2,14 +2,13 @@ import Navbar from '@/components/Navbar'
 import Menu from '@/components/Menu'
 
 export default function Home() {
-
     return (
         <div>
             <Navbar />
             <div className="flex">
                 <Menu />
                 <div className="flex items-center justify-center flex-1 bg-black h-screen">
-                    <h1 className="text-white text-2xl">HOME!</h1>
+                    <h1 className="text-white text-2xl">Funcionários!</h1>
                 </div>
             </div>
         </div>
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,29 @@
 export default function Menu() {
+<<<<<<< HEAD
     return <div className="bg-yellow-700">MENU COMPONENT</div>
+=======
+    return (
+        <div className="bg-gray-600 w-48 h-screen flex flex-col items-center pt-4">
+            <h1 className="font-bold text-white mb-2">MENU</h1>
+            <a href="/equipamentArea" className="text-white mt-6">
+                Equipamentos
+            </a>
+            <a href="/storageArea" className="text-white mt-6">
+                Estoque
+            </a>
+            <a href="/funcionaryArea" className="text-white mt-6">
+                Funcionários
+            </a>
+            <a href="/machinaryArea" className="text-white mt-6">
+                Maquinário
+            </a>
+            <a href="/cultivationArea" className="text-white mt-6">
+                Cultivo
+            </a>
+            <a href="/resourseManagementArea" className="text-white mt-6">
+                Tarefas
+            </a>
+        </div>
+    )
+>>>>>>> 4a5c4a3 (Salvando alterações antes de mudar para Home)
 }
