6ea8bab05130e0b468ae043399b34fb33a60dbe5
Vitor Hugo Paschuanelli <vitorhpaschuanelli@gmail.com>
2025-05-20 00:07:56 -0300

    Area update form creation, Navbar with Farm name

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,7 +2,7 @@ export default function Menu() {
     return (
         <div className="w-48 h-screen bg-gray-600 flex flex-col items-center pt-4">
             <h1 className="font-bold text-white mb-2">MENU</h1>
-            <a href="/areaControl" className="text-white mt-6">
+            <a href="/areaCreation" className="text-white mt-6">
                 Áreas
             </a>
             <a href="/farmEmployeeControl" className="text-white mt-6">
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,31 @@
+'use client'
+
+import React from 'react'
+import {FimComboBox} from './FimComboBox'
+import {ListBoxItem} from 'react-aria-components'
+
 export default function Navbar() {
+    const FarmOptions = [
+        {id: 60, name: 'Fazenda azul'},
+        {id: 61, name: 'Fazenda verde'},
+        {id: 62, name: 'Fazenda amarela'},
+    ]
+
+    //const [farmId, setFarmId] = React.useState<Key | null>(null)
+
     return (
         <div className="bg-gray-700 flex items-center justify-between h-16 px-4">
             <a href="/home">
                 <img src="/sample.png" alt="Logo" className="h-10 w-10" />
             </a>
             <div className="flex-1 text-center">
-                <h1 className="font-bold text-white">NOME DA FAZENDA</h1>
+                <FimComboBox
+                    defaultItems={FarmOptions}
+                    //onSelectionChange={setFarmId}
+                    allowsCustomValue
+                >
+                    {(item) => <ListBoxItem>{item.name}</ListBoxItem>}
+                </FimComboBox>
             </div>
             <img src="/user-account.png" alt="Usuário" className="h-10 w-10" />
         </div>
