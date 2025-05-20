fa2146c1b42e45b7396318ed964a933664454ea3
MiragemS <musantos016@gmail.com>
2025-05-20 17:54:45 -0300

    Update: table e estrutura do update forms

--- src/app/designTest/page.tsx
+++ src/app/designTest/page.tsx
@@ -12,18 +12,7 @@ export default function designTest() {
             <div>
                 <h1 className="text-xl font-bold mb-4">Lista de Fazendas</h1>
                 <AriaTable tipo="farm" dados={farmList!}></AriaTable>
-                <AriaTable tipo="generalFarms" dados={farmList!}></AriaTable>
             </div>
         </div>
     )
 }
-
-// TailwindPlay
-// <div class="flex h-screen w-screen flex-col">
-//     <div class="h-20 w-screen bg-blue-400">NAVBAR</div>
-//     <div class="flex grow flex-row">
-//         <div class="w-32 bg-green-400">SIDE CONTENT</div>
-//         <div class="grow bg-amber-400">MAIN CONTENT</div>
-//     </div>
-//     <div class="h-20 w-screen bg-red-400">FOOTER</div>
-// </div>
