72480747538a009ad54cecc5bf9bee82c8cebcd6
MiragemS <musantos016@gmail.com>
2025-05-20 20:30:10 -0300

    create test

---
+++ src/app/createTest/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import CreateFormFarm from '@/components/CreateTable'
+
+export default function createTest() {
+    return (
+        <div className="flex justify-center items-center">
+            <CreateFormFarm />
+        </div>
+    )
+}
--- src/app/designTest/page.tsx
+++ src/app/designTest/page.tsx
@@ -2,6 +2,7 @@
 
 import {FarmManagement} from '@/classes/FarmManagements'
 import {AriaTable} from '@/components/Table'
+import {Button} from 'react-aria-components'
 
 const farmManagement = new FarmManagement()
 const farmList = await farmManagement.listAllFarmsByFarmer('1')
@@ -10,7 +11,17 @@ export default function designTest() {
     return (
         <div className="flex justify-center items-center">
             <div>
-                <h1 className="text-xl font-bold mb-4">Lista de Fazendas</h1>
+                {/* Container com justify-between e largura total */}
+                <div className="flex justify-between items-center w-full mb-4 pt-4">
+                    <h1 className="text-xl font-bold">Lista de Fazendas</h1>
+                    <Button
+                        onPress={() => (window.location.href = '/createTest')}
+                        className="w-40 px-1 py-3 rounded-md shadow-md bg-green-600 hover:bg-green-700 text-white font-semibold"
+                    >
+                        Adicionar
+                    </Button>
+                </div>
+
                 <AriaTable tipo="farm" dados={farmList!}></AriaTable>
             </div>
         </div>
