d58351f0de62b263b8f2bcd3bec888d6420d6a80
MiragemS <musantos016@gmail.com>
2025-05-24 11:02:53 -0300

    Update: estilização sideBar

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,35 +1,104 @@
+'use client'
+import Image from 'next/image'
+import {Link} from 'react-aria-components'
+
 export default function Menu() {
     return (
         <div className="w-64 bg-gray-800 text-white min-h-screen flex flex-col shadow-lg">
-            <nav className="flex-1 p-4 space-y-2">
-                <h1 className="text-gray-400 uppercase text-s font-semibold px-4 py-2 mb-2">
-                    MENU
+            <div className="p-6 text-center text-lg font-normal">
+                <h1 className="text-xl font-bold text-white font-inter">
+                    GERENCIAMENTO
                 </h1>
-                <a
+            </div>
+            <nav className="flex-2 p-4 pt-3 space-y-2">
+                <h1 className="text-gray-400 uppercase text-xs font-semibold px-4 py-2 mb-2">
+                    PRINCIPAL
+                </h1>
+                <Link
+                    href="/"
+                    className="flex items-center px-4 py-3 rounded-md transition-colors hover:bg-green-600"
+                >
+                    <Image
+                        src="/img/dashboard.png"
+                        width={18}
+                        height={18}
+                        alt="Dashboard"
+                        className="mr-3"
+                    />
+                    <span>Dashboard</span>
+                </Link>
+                <Link
                     href="/areaControl"
                     className="flex items-center px-4 py-3 rounded-md transition-colors hover:bg-green-600"
                 >
+                    <Image
+                        src="/img/location.png"
+                        width={18}
+                        height={18}
+                        alt="Tarefas"
+                        className="mr-3"
+                    />
                     <span>Áreas</span>
-                </a>
-                <a
+                </Link>
+                <Link
                     href="/farmEmployeeControl"
                     className="flex items-center px-4 py-3 rounded-md transition-colors hover:bg-green-600"
                 >
+                    <Image
+                        src="/img/employees.png"
+                        width={18}
+                        height={18}
+                        alt="Trabalhadores"
+                        className="mr-3"
+                    />
                     <span>Funcionários</span>
-                </a>
-                <a
+                </Link>
+                <Link
                     href="/machineryControl"
                     className="flex items-center px-4 py-3 rounded-md transition-colors hover:bg-green-600"
                 >
+                    <Image
+                        src="/img/tractor.png"
+                        width={18}
+                        height={18}
+                        alt="Maquinários"
+                        className="mr-3"
+                    />
                     <span>Maquinários</span>
-                </a>
-                <a
+                </Link>
+                <Link
                     href="/resourseManagementControl"
                     className="flex items-center px-4 py-3 rounded-md transition-colors hover:bg-green-600"
                 >
+                    <Image
+                        src="/img/toDoList.png"
+                        width={18}
+                        height={18}
+                        alt="Tarefas"
+                        className="mr-3"
+                    />
                     <span>Tarefas</span>
-                </a>
+                </Link>
             </nav>
+            <div className="p-4 border-t border-gray-700 space-y-2">
+                <h1 className="text-gray-400 uppercase text-xs font-semibold px-4 py-2 mb-2">
+                    FERRAMENTAS
+                </h1>
+                <Link
+                    href="/"
+                    className="flex items-center px-4 py-3 rounded-md transition-colors hover:bg-green-600"
+                >
+                    <Image
+                        src="/img/setting.png"
+                        width={18}
+                        height={18}
+                        alt="Configurações"
+                        className="mr-3"
+                    />
+
+                    <span>Configurações</span>
+                </Link>
+            </div>
         </div>
     )
 }
