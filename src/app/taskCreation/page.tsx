f145a11a293cf97899f0b58c04c099aa54a85f70
Andre Badur <andrebadur123@gmail.com>
2025-06-24 19:27:22 -0300

    testing

--- src/app/taskCreation/page.tsx
+++ src/app/taskCreation/page.tsx
@@ -76,17 +76,6 @@ export default function TaskCreation() {
 
                     window.location.href = '/taskControl'
                 }}>
-                <TextField name="supply_quantity" isRequired>
-                    <Label className="block text-sm font-medium text-black-700 mb-1">
-                        Quantidade de Insumo
-                    </Label>
-                    <Input
-                        type="number"
-                        className="w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent"
-                    />
-                    <FieldError />
-                </TextField>
-
                 <TextField name="conclusion_date" isRequired>
                     <Label className="block text-sm font-medium text-black-700 mb-1">
                         Data de Conclusão
@@ -118,11 +107,24 @@ export default function TaskCreation() {
                     name="id_supply"
                     className="w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent">
                     {supplies.map((supply) => (
-                        <option key={supply.supply_id} value={supply.supply_id}>
-                            {supply.supply_category}
+                        <option
+                            key={supply.supply_categories.category_id}
+                            value={supply.supply_id}>
+                            {supply.supply_categories.category_name}
                         </option>
                     ))}
                 </select>
+
+                <TextField name="supply_quantity" isRequired>
+                    <Label className="block text-sm font-medium text-black-700 mb-1">
+                        Quantidade de Insumo
+                    </Label>
+                    <Input
+                        type="number"
+                        className="w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent"
+                    />
+                    <FieldError />
+                </TextField>
 
                 <Label className="block text-sm font-medium text-black-700 mb-1">
                     Máquina
